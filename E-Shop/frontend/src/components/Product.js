15404a216373acf0bb98862673b54393fc82f96c
Samrat Hossain <samratcse17019@gmail.com>
2021-04-13 10:50:38 +0600

    Creat React Frontend

---
+++ E-Shop/frontend/src/components/Product.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {Card} from 'react-bootstrap'
+import Rating from './Rating'
+import { Link } from 'react-router-dom'
+
+function Product({product}) {
+    return (
+        <Card className="my-3 p-3 rounded">
+            <Card.Body>
+                <Link to={`/product/${product._id}`}>
+                    <Card.Img src={product.image} />   
+                </Link>              
+                <Card.Title as="div">
+                    <Link to={`/product/${product._id}`}>
+                        <strong>{product.name}</strong>  
+                    </Link>
+                    
+                </Card.Title>
+                
+                <Card.Text as="div">
+                    <div className="my-3">
+                        <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
+                    </div>
+                </Card.Text>
+
+                <Card.Text as="h3">
+                    ${product.price}
+                </Card.Text>
+
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default Product
